991df794694959f9c57e607e3f0db3e09e4b4566
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-01-16 17:46:43 +0530

    animation on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -59,4 +59,30 @@ $(document).ready(function () {
     }
   });
 
+
+  //animation on scroll
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+            offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+            offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+            offset: '50%'
+    });
+
 });
